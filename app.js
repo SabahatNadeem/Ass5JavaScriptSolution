471ed9e268448c2e9af299c3260bfc1cb2cfd694
SabahatNadeem <sabahatnadeem184@gmail.com>
2020-06-04 23:45:16 +0500

    till chp 5

---
+++ app.js
@@ -0,0 +1,99 @@
+
+/*//Chp 01
+alert("Welcome");
+alert("Error! Please enter valid password!");
+alert("Welcome to JS land...\nHappy coding to you!",);
+alert("Welcome to JS land");
+alert("Prevent this page from creating additional dialogs");
+console.log("Hello.. I can run JS throgh my web browser's console");
+
+//Chp 02
+var username="";
+var myName="Sabahat Nadeem";
+var message="Hello World!";
+alert(message);
+var fname="John Doe";
+var age="15 years old";
+var qual="Certified Mobile Application Development";
+alert(fname);
+alert(age);
+alert(qual);
+var food="PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(food);
+var email="example@example.com";
+alert("My Email Address is "+email);
+var book="“A smarter way to learn JavaScript";
+alert("I am trying to learn from th book "+ book);
+document.write("Yah! I can write HTML content through JavaScript");
+var pattern="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(pattern);
+
+// Chp 03
+var age1=19;
+alert("I am "+age1+" years old!");
+var numberOfVisits=0;
+numberOfVisits=numberOfVisits+1;
+alert("You have visited the website "+numberOfVisits+ " times");
+var birthYear=2001;
+document.write("My birth year is "+birthYear);
+document.write("Data type of variable birthYear is "+typeof(birthYear));
+var visitorName="John Doe";
+var product="T-Shirt";
+var quantity=05;
+document.write(visitorName +" ordered" + quantity+ " "+ product +" "+ " on XYZ Clothing store"); 
+
+//Chp 04
+
+var name,fullName,middleName="";
+//var @fullName,2Name,="";
+
+document.write("Variable names can only contain numbers,$,letters and underscores.For example $my_1stVariable<br/>");
+document.write("Variables must begin with a letter, $ or underscore. For example $name, _name or name<br/>");
+document.write("Variable names are case sensitive<br/>");
+document.write("Variable names should not be JS keywords<br/>");
+
+
+//Chp 05
+
+var a=parseInt(prompt("a"));
+var b=parseInt(prompt("b"));
+var c=a+b;
+var d=a-b;
+var e=a*b;
+var f=a/b;
+var g=a%b;
+document.write("sum of " + a + " and " + b + " is " + c + "<br/>");
+document.write("subtraction of " + a + " and " + b + " is " + d + "<br/>");
+document.write("multiplication of " + a + " and " + b + " is " + e + "<br/>");
+document.write("division of " + a + " and " + b + " is " + f + "<br/>");
+document.write("modulus of " + a + " and " + b + " is " + g + "<br/>");
+
+var number=0;
+document.write("Value after variable declaration is:" + number + "<br/>");
+number=6;
+document.write("Initial value:" + number + "<br/>");
+number++;
+document.write("Value after increment is: " + number + "<br/>");
+number=number+7;
+document.write("Value after addition is " + number + "<br/>");
+number--;
+document.write("Value after decrement  is " + number + "<br/>");
+number=number%3;
+document.write("The remainder is : " + number + "<br/>");
+var ticket=600;
+ticket=ticket*5;
+document.write("Total cost of buying 5 tickets is " + ticket + " PKR " + "<br/>");
+document.write("Table Of 5 <br/> ");
+var table=5;
+for(var x=1; x<=10; x++)
+{
+    document.write(table + "* " + x + " = " + table*x + " <br/>");
+}
+var tempC=25;
+var f=70;
+var tempF=(tempC*9/5)+32;
+tempF=(tempC*9/5)+32;
+tempCent=(f-32)*(5/9);
+document.write(tempC + "C " + " is" + tempF + " F <br/>");
+document.write(f + "C " + " is" + tempCent + " C <br/>");
+*/
